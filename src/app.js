65a54a2ae7d96bf52d0117ce69133c529f93823c
umairx97 <umair.bajwa@hotmail.com>
2019-01-09 23:41:53 +0500

    Practicing

--- src/app.js
+++ src/app.js
@@ -1,63 +1,13 @@
-import React, {Component} from 'react'; 
-import App2 from './app2';
-import Cities from './cities'
+import React from 'react'; 
 
-
-
-class App extends Component { 
-    
-    state = { 
-        CityName: ['Karachi', 'Quetta', 'Lahore']
-    }
-
-
-
-    // state = { 
-    //     CityName: ['karachi', 'quetta', 'lahore'],
-    //     address: 'axiom'
-    // }
-
-    // ChangeState = this.setState(
-    //     (ps) => ({ 
-    //         CityName: ['abc']
-    //     })
-    // )
-
-
-    ChangeState = () => { 
-        this.setState({
-            CityName: ['Umair', 'Asad', 'Shahbaz']
-        })
-    }
-    // ChangeState = () => { 
-    //     this.setState({
-    //         CityName: ['abc'],
-    //     })
-    // }
-    
-
-    render(){
-    
+class App extends React.Component { 
+    render() { 
         return ( 
             <div>
-
-                <Cities anyname = {this.state.CityName} changethestate = {this.ChangeState} />
-                {/* <App2/> */}
-                
+                hello
             </div>
         )
     }
 }
+
 export default App;
-
-// import React from 'react'; 
-
-// const App = () => { 
-//     return ( 
-//         <div>
-//             hello
-//         </div>
-//     )
-// }
-
-// export default App;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,10 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import App3 from './app3';
 
-class Display extends Component {
-    render(){
-        return ( 
-            <div>
-                <App/>
 
-                {/* <App3 value = {'hello'}/> */}
-            </div>
-            
-        )
-    } 
 
-}
-// const Display = () => { 
-//     return ( 
-//         <div>
-//             hello
-//         </div>
-//         // <App/>
-//     )
-// }
-
-ReactDOM.render(<Display/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 if (module.hot) { 
     module.hot.accept();
