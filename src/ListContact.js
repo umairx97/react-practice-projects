7b08bc4f9ae45b79099c89652eec6a5bcc24f641
umairx97 <umair.bajwa@hotmail.com>
2019-01-10 23:32:41 +0500

    Contacts App

---
+++ src/ListContact.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'; 
+
+class ListContacts extends Component { 
+    render() { 
+
+        console.log(this.props);
+        return ( 
+            <ol className = "contact-list ">
+
+
+
+            </ol>
+        )
+    }
+}
+
+export default ListContacts;
--- src/app.js
+++ src/app.js
@@ -1,32 +1,36 @@
 import React from 'react'; 
+import ListContacts from './ListContact';
 
+const contacts = [
+    {
+      "id": "karen",
+      "name": "Karen Isgrigg",
+      "handle": "karen_isgrigg",
+      "avatarURL": "http://localhost:5001/karen.jpg"
+    },
+    {
+      "id": "richard",
+      "name": "Richard Kalehoff",
+      "handle": "richardkalehoff",
+      "avatarURL": "http://localhost:5001/richard.jpg"
+    },
+    {
+      "id": "tyler",
+      "name": "Tyler McGinnis",
+      "handle": "tylermcginnis",
+      "avatarURL": "http://localhost:5001/tyler.jpg"
+    }
+   ];
+
+
+   
 class App extends React.Component { 
     render() { 
-        const contacts = [
-            {
-              "id": "karen",
-              "name": "Karen Isgrigg",
-              "handle": "karen_isgrigg",
-              "avatarURL": "http://localhost:5001/karen.jpg"
-            },
-            {
-              "id": "richard",
-              "name": "Richard Kalehoff",
-              "handle": "richardkalehoff",
-              "avatarURL": "http://localhost:5001/richard.jpg"
-            },
-            {
-              "id": "tyler",
-              "name": "Tyler McGinnis",
-              "handle": "tylermcginnis",
-              "avatarURL": "http://localhost:5001/tyler.jpg"
-            }
-           ];
-
+      
 
         return ( 
             <div>
-                hello
+                <ListContacts contacts = {contacts}/>
             </div>
         )
     }
