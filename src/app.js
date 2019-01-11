dd537e837a85ad1a5296167c9e397cd2a4df6ac8
umairx97 <umair.bajwa@hotmail.com>
2019-01-11 21:29:47 +0500

    Contacts app

--- src/app.js
+++ src/app.js
@@ -1,51 +1,58 @@
-import React from 'react'; 
+import React from 'react';
 import ListContacts from './ListContact';
 
-class App extends React.Component { 
+class App extends React.Component {
 
-    state = { 
+    state = {
         contacts: [
             {
-              "id": "Michael",
-              "name": "Michael Isgrigg",
-              "handle": "@Michael_isgrigg",
-              "avatarURL": "http://localhost:5001/michael.jpg"
+                id: '1',
+                name: 'Umair Ahmed Bajwa',
+                Occupation: 'Developer',
+                handle: '@umairx97',
+                Age: 21,
+                Location: 'Karachi',
+                avatarURL: 'http://localhost:5001/michael.jpg'
             },
             {
-              "id": "ryan",
-              "name": "ryan Kalehoff",
-              "handle": "@ryan_kalehoff",
-              "avatarURL": "http://localhost:5001/ryan.jpg"
+                id: '2',
+                name: 'Asad Ikhlas',
+                handle: '@asadIkhlas03',
+                Occupation: 'Developer',
+                Age: 22,
+                Location: 'Karachi',
+                avatarURL: 'http://localhost:5001/ryan.jpg'
             },
             {
-              "id": "tyler",
-              "name": "Tyler McGinnis",
-              "handle": "@tylermcginnis",
-              "avatarURL": "http://localhost:5001/tyler.jpg"
-            }, 
+                id: '3',
+                name: 'Shahbaz Hussain',
+                Occupation: 'Developer',
+                handle: '@Shahbazhussain97',
+                Age: 23,
+                Location: 'Karachi',
+                avatarURL: 'http://localhost:5001/tyler.jpg'
+            },
 
         ]
     }
+    removeContact = (contacts) => {
+        this.setState((currentState) => ({
+            contacts: currentState.contacts.filter((c) => {
+                return c.id !== contacts.id
+            })
+
+        }))
+
+    }
+
+    render() {
 
 
-    // removeContact = (contacts) => {
-    //     this.setState((currentState)=> ({ 
-    //         contacts: currentState.contacts.filter((c)=> {
-    //             return c.id !== contacts.id
-    //         })
-
-    //     }))
-
-    // }
-
-    render() { 
-      
-
-        return ( 
+        return (
             <div>
-                {/* <ListContacts contacts = {this.state.contacts}
+                <ListContacts contacts = {this.state.contacts}
                 onDelete = {this.removeContact}
-                /> */}
+                />
 
 
             </div>
