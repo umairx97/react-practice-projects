ef78643c77424913d91045ee2c1eb04302295ca7
umairx97 <umair.bajwa@hotmail.com>
2019-01-12 10:45:47 +0500

    Practice

--- src/practice.js
+++ src/practice.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-const Practice = () => {
-    return( 
-        <div>
-            hello
-        </div>
-    )
+// const Practice = () => {
+//     return( 
+//         <div>
+//             hello
+//         </div>
+//     )
+// }
+
+class Practice extends React.Component {
+    render() { 
+        return ( 
+            <div>
+                hello
+            </div>
+        )
+    }
 }
 
 export default Practice;
