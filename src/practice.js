b1de41b0c4e1e0f0eb044261ed0642013930762c
umairx97 <umair.bajwa@hotmail.com>
2019-01-12 13:07:10 +0500

    React Class 3 before break

--- src/practice.js
+++ src/practice.js
@@ -1,29 +1,68 @@
-import React, {Component} from 'react'
-import axios from 'axios';
+import React, { Component } from 'react'
+
+
+class App2 extends Component {
+    
+    
+    state = { 
+        text: this.props.num
+    }
+
+
+    static getDerivedFromProps(props){ 
+        return ({ 
+            text: props.num
+        })
+    }
+
+    render() { 
+
+        return ( 
+            <div>
+                <h2>This is the 2nd Component</h2>
+                {`the State number is ${this.state.text}`}
+            </div>
+        )
+    }
+}
+
 
 class Practice extends Component {
 
+
+
     state = {
-        persons: [],
-      }
-    
-      componentDidMount() {
-        axios.get(`http://localhost:3001/posts`)
-          .then(res => {
-            const persons = res.data;
-            this.setState({ persons });
-          })
+       number: 0, 
+    }
 
-      }
+
+    // constructor(){ 
+    //     super()
+    //     this.state.number = 100
+    // }
+    componentDidMount = () => {
+        setTimeout(() => { 
+            this.setState(() => ({ 
+                number: Math.floor(Math.random() * 1000)
+            }))
+        }, 1000)
+
+        // this.setState(() => ({ 
+        //     number: Math.ceil(Math.random() * 1000),
+        // }))
+      
+    }
     
-      render() {
+
+
+    render() {
+
         return (
-          <ul>
-
-            { this.state.persons.map(person => <li key = {person.id}>{person.title}</li>)}
-          </ul>
+            <div> 
+                <App2 num = {this.state.number}/>
+            </div>
         )
-      }
+    }
 }
 
 export default Practice;
