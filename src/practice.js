2145604906c2aeffbc6168c32cd0308f83b91c68
umairx97 <umair.bajwa@hotmail.com>
2019-01-14 03:02:02 +0500

    Migrated to thn-app

--- src/practice.js
+++ src/practice.js
@@ -1,51 +1,8 @@
 import React, { Component } from "react";
 
-const list = [
-  {
-    title: "React",
-    url: "https://facebook.github.io/react/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  }
-];
-
 class Practice extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      list: list
-    };
-  }
-
   render() {
-    return (
-      <div className="App">
-        {this.state.list.map(item => (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>Title: {item.title} </a>
-            </span>
-            <br />
-            <span>Author: {item.author}</span> <br />
-            <span>Comments: {item.num_comments}</span>
-            <br />
-            <span>Upvotes: {item.points}</span>
-            <span>
-              {" "}
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type="button"
-              >
-                Dismiss
-              </button>
-            </span>
-          </div>
-        ))}
-      </div>
-    );
+    return <div>hello</div>;
   }
 }
 
