c4d3685c104d56f630db069ae5837aba06bdad37
umairx97 <umair.bajwa@hotmail.com>
2019-01-14 01:32:14 +0500

    Book Application

--- src/practice.js
+++ src/practice.js
@@ -1,20 +1,41 @@
 import React, { Component } from "react";
 
-const key = "abc";
-const user = {
-  [key]: "Robin"
-};
-
-console.log(key);
-console.log(user);
+const list = [
+  {
+    title: "React",
+    url: "https://facebook.github.io/react/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  }
+];
 
 class Practice extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      list: list
+    };
   }
 
   render() {
-    return <div>hello</div>;
+    return (
+      <div className="App">
+        {this.state.list.map(item => (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>Title: {item.title} </a>
+            </span>
+            <br />
+            <span>Author: {item.author}</span> <br />
+            <span>Comments: {item.num_comments}</span><br/>
+            <span>Upvotes: {item.points}</span>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
