7f623bc7704e348d0ff006f4016a00ba1a7c25af
umairx97 <umair.bajwa@hotmail.com>
2019-01-14 00:04:20 +0500

    Commit

--- src/practice.js
+++ src/practice.js
@@ -1,31 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-
-
-const key = 'abc';
+const key = "abc";
 const user = {
-    [key]: 'Robin'
-}
+  [key]: "Robin"
+};
 
-console.log(key)
-console.log(user)
-
+console.log(key);
+console.log(user);
 
 class Practice extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
 
-
-    }
-
-    render() {
-
-        return (
-            <div>
-                hello
-            </div>
-        )
-    }
+  render() {
+    return <div>hello</div>;
+  }
 }
 
 export default Practice;
