5a50aa2bb452d19c81ce2c518ec2a34d44a7fdd0
umairx97 <umair.bajwa@hotmail.com>
2019-01-17 19:50:09 +0500

    Practice

--- src/practice.js
+++ src/practice.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 
+
+const key = 'name'; const user = { [key]: 'Robin', };
+
+
 class Practice extends Component {
   render() {
     return <div>hello</div>;
