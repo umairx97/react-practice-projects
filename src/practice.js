f3b641f46c4a6cb220a70de140cc2faabe851984
umairx97 <umair.bajwa@hotmail.com>
2019-01-17 23:13:22 +0500

    commit

--- src/practice.js
+++ src/practice.js
@@ -1,12 +1,43 @@
 import React, { Component } from "react";
 
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+  }
 
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
 
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
 
-class Practice extends Component {
+  tick = ()=> {
+    this.setState({
+      date: new Date()
+    });
+  }
+
   render() {
-    return <div>hello</div>;
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
   }
 }
+
+const Practice = () => {
+  return (
+    <div>
+      <Clock/>
+    </div>
+  );
+};
 
 export default Practice;
